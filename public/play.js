e8c6e5bead7f6b4cd70a8928f6dec0ff9b64cae6
showloser <ngweiming2004@gmail.com>
2025-02-04 21:19:29 +0800

    too add in endGame animations

--- public/play.js
+++ public/play.js
@@ -431,7 +431,6 @@ function restart(){
 
 function gameEnd(){
     GameEndCardAnimation()
-
     cardShuffleAnimation()
 }
 
@@ -590,6 +589,33 @@ socket.on('connect', () => {
     })
 
     socket.on('gameEnd' , (outcome) => {
+
+
+        function showBanner(outcome) {
+            const banner = document.querySelector('.banner');
+        
+            outcome.forEach((data) => {
+                const key = Object.keys(data)[0]; // Get the key of the object
+                if (key == clientPlayerId){
+        
+                    if (data[key].playerBalance  > 1000){
+                        banner.className = 'banner win';
+                        banner.querySelector('h2').textContent = `VICTORY! You Have Won $${data[key].betAmount}`;
+                    }
+                    else{
+                        banner.className = 'banner lose';
+                        banner.querySelector('h2').textContent = `Nice Try! You Have Lost $${data[key].betAmount}`;
+        
+                    }
+        
+                }
+            })
+        }
+
+
+        showBanner(outcome)
+
+
         //function to move cards back to deck
         GameEndCardAnimation()
         
